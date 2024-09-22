818812194ea81689b1467ea8990f18d89143d3f6
Blurr28 <jeeveshsharma28@gmail.com>
2024-09-23 00:23:55 +0530

    added implmentation for integers bencode encoding/decoding

---
+++ bencode/integers.h
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef INTEGERS_H
+#define INTEGERS_H
+
+int decode_integers(string str);
+
+string encode_integers(int integer_value);
+
+#endif // !INTEGERS_H
