d613786e9b29cef2c4b8dc5e8915cea3c79630eb
Blurr28 <jeeveshsharma28@gmail.com>
2024-09-22 23:39:47 +0530

    implmented bencode encoding/decoding for byte_string

---
+++ bencode/byteStrings.h
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+#ifndef BYTESTRING_H
+#define BYTESTRING_H
+
+string decode_byte_string(string str);
+
+string encode_byte_string(string str);
+
+#endif // !BYTESTRING_H
